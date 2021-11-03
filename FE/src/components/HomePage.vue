21e22bac2c28a20345d0344b001ae2cc0ef94533
Matias Grimm <matiasgrimm@gmail.com>
2021-11-03 13:31:01 +0100

    Update HomePage.vue
    Changed what items is being showed from the product bought fewest times to most times.
    
    Changed from vertical list to horizontal list

--- FE/src/components/HomePage.vue
+++ FE/src/components/HomePage.vue
@@ -1,12 +1,21 @@
 <template>
   <v-container fluid>
     <div v-if="sortedDummyData.length > 1">
-      <!-- <h4>{{ text }}</h4> -->
-      <!-- <br /> -->
-      <!-- <h3>Most popular items:</h3> -->
-      <v-row>
-        <v-col cols="12" sm="6" md="6" lg="4" xl="3" v-for="(item, index) in sortedDummyData" :key="index" class="mb-8 px-4 text-center">
-          <v-card class="d-flex flex-column" style="height: 100%">
+      <h3>Top 10 most popular products</h3>
+
+      <v-slide-group show-arrows class="pa-4">
+        <template v-slot:[`prev`]>
+          <v-btn icon>
+            <v-icon :class="hover ? (size = '30px') : (size = '10px')">mdi-chevron-left</v-icon>
+          </v-btn>
+        </template>
+        <template v-slot:[`next`]>
+          <v-btn icon>
+            <v-icon :class="hover ? (size = '30px') : (size = '10px')">mdi-chevron-right</v-icon>
+          </v-btn>
+        </template>
+        <v-slide-item v-for="(item, index) in sortedDummyData" :key="index" class="mb-8 px-4">
+          <v-card class="d-flex flex-column mx-1 my-1 pa-0" style="height: 100%" elevation="2" tile>
             <v-card-title class="text-center d-block text-h5 text-xl-h4">
               {{ item.ProductTitle }}
             </v-card-title>
@@ -31,67 +40,22 @@
               </div>
             </v-card-text>
           </v-card>
-        </v-col>
-      </v-row>
+        </v-slide-item>
+      </v-slide-group>
     </div>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'HomePage',
 
   data: () => ({
     // This is test data
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus nisi ligula, ac ultricies erat aliquam finibus. Ut rutrum eu felis vel finibus. ' +
-      'Aenean dictum sodales pellentesque. Nunc viverra tellus eu ante fringilla pulvinar. Vestibulum sed mollis diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.' +
-      'Integer vitae metus tempus, blandit tortor nec, semper diam. Curabitur nec cursus urna. Suspendisse laoreet, leo a luctus malesuada, ipsum orci tristique felis, vel iaculis diam elit vel enim. In finibus dignissim est in elementum.' +
-      'Fusce dictum pharetra porttitor. Ut hendrerit ultrices tellus id fermentum. Praesent a mauris augue. Donec velit ex, laoreet ac convallis nec, laoreet sed risus. ' +
-      'Ut nulla dolor, ornare sed velit eget, condimentum tincidunt ipsum. Nulla vel velit non diam ultricies suscipit. Curabitur molestie semper est ut euismod. Morbi et risus lorem. Vestibulum interdum nulla nec pretium elementum. ' +
-      'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque blandit sodales quam. Fusce interdum mauris non dapibus elementum. Phasellus in blandit elit, sed mollis nibh. ' +
-      'Etiam vitae nisl a est blandit lacinia suscipit nec nulla. Morbi volutpat porta nunc quis congue. Fusce dictum molestie est et faucibus. Nulla facilisi. Vestibulum blandit gravida elit eget consequat. ' +
-      'Quisque eget justo eu ex pulvinar consectetur ut ac quam. Maecenas pharetra, odio nec laoreet aliquam, ex enim feugiat massa, a pharetra dui nisi efficitur leo. Praesent odio dolor, feugiat eu neque eu, commodo finibus est. ' +
-      'Duis quis nisl tempus, sagittis nisi quis, sagittis justo. Vestibulum elementum ut erat a aliquam.',
     sortedDummyData: [],
-    dummyData: [
-      {
-        ProductId: 1,
-        ProductTitle: 'Product 1',
-        ProductDescription: 'This is a test description',
-        ProductPrice: 250,
-        ProductStock: 7,
-        ProductBought: 251,
-        ProductDiscount: 5,
-      },
-      {
-        ProductId: 2,
-        ProductTitle: 'Product 2',
-        ProductDescription: 'This is a test description',
-        ProductPrice: 225,
-        ProductStock: 2,
-        ProductBought: 663,
-        ProductDiscount: 7.5,
-      },
-      {
-        ProductId: 3,
-        ProductTitle: 'Product 3',
-        ProductDescription: 'This is a test description',
-        ProductPrice: 5551,
-        ProductStock: 3,
-        ProductBought: 17,
-        ProductDiscount: 10,
-      },
-      {
-        ProductId: 4,
-        ProductTitle: 'Product 4',
-        ProductDescription: 'This is a test description',
-        ProductPrice: 1932,
-        ProductStock: 543,
-        ProductBought: 1543,
-        ProductDiscount: 15,
-      },
-    ],
+    dummyData: [],
     headersForDummyData: [
       {
         text: 'Product',
@@ -117,11 +81,13 @@ export default {
   }),
 
   computed: {
+    ...mapGetters(['dummyShopItems']),
     averageSold() {
       return this.$util.calculateAverageSoldItems(this.dummyData);
     },
   },
   methods: {
+    // This method will get only top 8 most bought products, sorted by how many times product has been bought, and if that's equal to another then the stock of products will be compared.
     checkBoughtNumber() {
       let productCount = 0;
       this.dummyData.forEach((item) => {
@@ -130,9 +96,12 @@ export default {
           productCount++;
         }
       });
+      this.sortedDummyData.sort((a, b) => (a.ProductBought < b.ProductBought ? 1 : a.ProductBought == b.ProductBought ? (a.ProductStock > b.ProductStock ? 1 : -1) : -1));
     },
   },
   created() {
+    this.dummyData = this.dummyShopItems;
+    // this.$store.dispatch('fetch_items');
     this.checkBoughtNumber();
   },
 };
