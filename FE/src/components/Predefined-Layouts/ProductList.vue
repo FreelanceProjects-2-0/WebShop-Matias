729d5940b8ba9371823a57c02a508bab1fb7e544
Matias Grimm <matiasgrimm@gmail.com>
2021-11-08 07:58:17 +0100

    Update ProductList.vue

--- FE/src/components/Predefined-Layouts/ProductList.vue
+++ FE/src/components/Predefined-Layouts/ProductList.vue
@@ -2,11 +2,46 @@
   <v-container fluid>
     <h1 v-if="errorMessage">{{ errorMessage }}</h1>
     <v-row no-gutters v-else>
-      <v-col cols="12" v-for="(item, index) in productList" :key="index" class="mb-2 px-4 text-left">
-        <v-card class="d-flex flex-column" style="height: 100%">
+      <v-col cols="12" v-for="(item, index) in productList" :key="index" class="mb-2 px-0 text-left">
+        <!--   -->
+        <v-card v-if="isMobile" class="" style="height: 100%">
           <v-row no-gutters>
             <v-col>
               <v-card-title>
+                {{ item.ProductTitle }}
+              </v-card-title>
+              <v-card-subtitle>
+                <v-row no-gutters>
+                  <v-col cols="9">
+                    {{ item.ProductDescription + 'ajsdjioj diosa jdoias jdoiasjdoiajoidajoidjasoisaoi djia jdoias jdisa jdoas' }}
+                    <div class="d-flex justify-end mt-0">
+                      <v-img width="250px" src="@/assets/placeholder.jpg"></v-img>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-card-subtitle>
+            </v-col>
+            <v-card-text class="text-xl-h5 text-lg-h6 text-subtitle-1 black--text">
+              <div class="d-flex">
+                <h4>Price&nbsp;${{ $util.dicountCalculator(item.ProductPrice, item.ProductDiscount) }}</h4>
+                <h4 class="ml-auto text-decoration-line-through grey--text">${{ item.ProductPrice }}</h4>
+              </div>
+              <div class="d-flex" v-if="item.ProductStock <= 50">
+                <h4>Items in stock&nbsp;</h4>
+                <h4 :class="$util.stockNumberColor(item.ProductStock)" class="ml-auto">{{ item.ProductStock }}</h4>
+              </div>
+              <div class="d-flex">
+                <h4>Items sold</h4>
+                <h4 class="ml-auto">{{ item.ProductBought }}</h4>
+              </div>
+            </v-card-text>
+          </v-row>
+        </v-card>
+        <!--  -->
+        <v-card v-else class="d-flex flex-column" style="height: 100%">
+          <v-row no-gutters>
+            <v-col>
+              <v-card-title class="text-xl-h4">
                 {{ item.ProductTitle }}
               </v-card-title>
               <v-card-subtitle class="text-xl-h5">
@@ -24,12 +59,11 @@
             </v-col>
             <v-card-text class="text-xl-h5 text-lg-h6 text-subtitle-1 black--text">
               <v-row no-gutters>
-                <v-col>
-                  <h4>Price&nbsp;</h4>
-                  <h4>${{ $util.dicountCalculator(item.ProductPrice, item.ProductDiscount) }}</h4>
+                <v-col class="">
+                  <h4>Price&nbsp;${{ $util.dicountCalculator(item.ProductPrice, item.ProductDiscount) }}</h4>
                   <h4 class="text-decoration-line-through grey--text">${{ item.ProductPrice }}</h4>
                 </v-col>
-                <v-col class="justify-end" cols="3">
+                <v-col class="justify-end" cols="6">
                   <div class="d-flex justify-end" v-if="item.ProductStock <= 50">
                     <h4 :class="$util.stockNumberColor(item.ProductStock)">{{ item.ProductStock }}</h4>
                     <h4>&nbsp;Items in stock</h4>
@@ -54,6 +88,16 @@ export default {
     productList: [],
     errorMessage: '',
   }),
+  computed: {
+    isMobile() {
+      console.log(window.innerWidth);
+      if (window.innerWidth < 350) {
+        return true;
+      }
+      return false;
+    },
+  },
+  methods: {},
   props: {
     data: Array,
   },
