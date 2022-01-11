9213937d90f8c1c4c99c357d487d451fd707305b
Matias2339 <matiasgrimm@gmail.com>
2022-01-11 18:09:12 +0100

    login should not change the current page.

--- FE/src/components/Login.vue
+++ FE/src/components/Login.vue
@@ -62,7 +62,7 @@ export default {
           .then(() => {
             this.isLoading = false;
             this.$emit("hide-login");
-            this.$router.push({name:'products'});
+            // this.$router.push({name:'frontPage'});
           })
           .catch(() => {
             this.isLoading = false;
